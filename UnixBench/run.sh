678f7aefd8773dd4c02f9cb5aab15f804f681112
ASI testing <ubuntu@ip-10-45-201-86.ec2.internal>
2014-10-13 09:35:32 +0000

    nits.

--- UnixBench/run.sh
+++ UnixBench/run.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-OUTPUT='/home/ubuntu/benchmarks/UnixBench/test_unixbench.txt'
-echo "=======" >> $OUTPUT
-date		>> $OUTPUT 
-echo "=======" >> $OUTPUT
 UBENCH_HOME=${HOME}/benchmarks/UnixBench/
+OUTPUT=${UBENCH_HOME}/test_unixbench.txt
+echo "=======" 	>> $OUTPUT
+date		>> $OUTPUT 
+echo "=======" 	>> $OUTPUT
+
+cd ${UBENCH_HOME}
 # Regular
 (time ${UBENCH_HOME}/Run) >>  $OUTPUT 2>&1
 
