2e1f7f64c69557e5583e4974a187350a77eb62e2
Kangkook Jee <kjee@nec-labs.com>
2014-10-14 15:09:37 -0400

    changes to scripts.

--- ubench_run.sh
+++ ubench_run.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 UBENCH_HOME=${HOME}/benchmarks/UnixBench/
-OUTPUT=${UBENCH_HOME}/test_unixbench.txt
-echo "=======" 	>> $OUTPUT
-date		>> $OUTPUT 
-echo "=======" 	>> $OUTPUT
+OUTPUT=${HOME}/benchmarks/logs/unixbench.txt
+echo "==== BEGIN: Unixbench ===="    >> $OUTPUT
+date >> $OUTPUT 
+echo "=========================="    >> $OUTPUT
 
 cd ${UBENCH_HOME}
 # Regular
-(time ${UBENCH_HOME}/Run) >>  $OUTPUT 2>&1
+(time ${UBENCH_HOME}/Run) >> $OUTPUT 2>&1
 
 # Short
 #./Run -i 1 | tee $OUTPUT
+
+echo "===== END: Unixbench ====="    >> $OUTPUT
+date >> $OUTPUT 
+echo "=========================="    >> $OUTPUT
